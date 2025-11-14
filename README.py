c5339ecd8629a9cc416eca07e5101fae442e5b81
HaseebNaich <haseebnaichmalik08@gmail.com>
2025-11-14 08:36:05 +0500

    Add README.py for user interaction and grading
    This script greets the user, displays the first 100 even numbers, and calculates grades based on input marks.

---
+++ README.py
@@ -0,0 +1,40 @@
+print ("Hy,Dear.")
+print ("My name is Haseeb ")
+name = input ("What is your name?")
+print ("Welcome ", name)
+print ("How are you ?:")
+print ("This is a program that displays first 100 even numbers.")
+num = 2
+while num <= 100 :
+    print ("Even number is : ",num)
+    num = num + 2
+print ("Now,We move to grading marks..")
+t_marks = float(input("Please,Enter your total marks"))
+print ("Your total marks are: ",t_marks )
+o_marks = float(input ("Please ❤️, Enter your obtained marks"))
+print("Your obtained marks are: ", o_marks)
+percentage = (o_marks / t_marks) * 100
+if percentage >= 90 :
+    print ("Congratulations ❤️❤️,Your grade is : A+")
+elif percentage >= 85 :
+    print ("Your grade is : A")
+elif percentage >= 80 :
+    print ("Your grade is : B+")
+elif percentage >= 75 :
+    print ("Your grade is : B ")
+elif percentage >= 70 :
+    print ("Your Grade is : C+")
+elif percentage >= 65 :
+    print ("Your Grade is : C")
+elif percentage >= 60 :
+    print ("Your Grade is : D+")
+elif percentage >= 50 :
+    print ("Your Grade is : D")
+elif percentage >= 40 :
+    print ("Your Grade is : F")
+else :
+    print ("Oops 🥲, You are Failed.")
+  print ("Are You Like it? 🥰 ")
+n = input ("Please Enter Your Answer ❤️❤️ : ")
+print ("Thanks for your Opinion.")
+  
